1f778f56b75ff365974b8d57f5c99b829a318f6a
Robert T. Stewart <bobstewart@goodtiming.com>
2016-11-17 15:27:35 -0500

    - adds data-sku attributes to all listing and cart item divs
    - this is intended to be used during dynamic page creation with javascript
    
    - made insignificant change to scroll-then-fix.js to make scrolledY value potentially more globally available
    
     On branch master
     Changes to be committed:
    	modified:   product-listing-page-v5.html
    	modified:   scripts/scroll-then-fix.js

--- scripts/scroll-then-fix.js
+++ scripts/scroll-then-fix.js
@@ -3,9 +3,11 @@
 /* https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop */
 
 var showCart = document.querySelector('.show-cart-listing');
-
+var scrolledY;
 window.addEventListener('scroll', function(e) {
-  console.log(window.scrollY);
+  // in case I want the scrolled value elsewhere
+  // scrolledY = window.scrollY;
+  // console.log(scrolledY);
   if (window.scrollY > 220) {
     showCart.classList.add('fix-show-cart');
   } else {
